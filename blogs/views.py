30fb7bb073541381157a29da8fcb785d55ec4933
blacksnow <domezaza48@gmail.com>
2022-05-11 11:32:21 +0700

    Deploy App to Heroku

---
+++ blogs/views.py
@@ -0,0 +1,69 @@
+import imp
+from urllib import request
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Post
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+
+# Create your views here.
+def hello(request):
+    # Query data from Model 
+    data = Post.objects.all()
+    return render(request, 'index.html', {'posts':data})
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def createForm(request):
+    return render(request, 'form.html')
+
+def loginForm(request):
+    return render(request, 'login.html')
+
+def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+
+    # login
+    # check username, password
+    user = auth.authenticate(username=username, password=password)
+    if user is not None :
+        auth.login(request, user)
+        return redirect('/')
+    else :
+        messages.info(request, 'ไม่พบข้อมูล')
+        return redirect('/loginForm')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def addUser(request):
+    username = request.POST['username']
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    email = request.POST['email']
+    password = request.POST['password']
+    repassword = request.POST['repassword']
+
+    if password==repassword : 
+        if User.objects.filter(username=username).exists() :
+            messages.info(request, 'username นี้มีผู้ใช้งานแล้ว')
+            return redirect('/createForm')
+        elif User.objects.filter(email=email).exists() :
+            messages.info(request, 'email นี้เคยลงทะเบียนแล้ว')
+            return redirect('/createForm')
+        else :
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+                first_name=firstname,
+                last_name=lastname
+            )
+            user.save()
+            return redirect('/')
+    else :
+        messages.info(request, 'password ไม่ตรงกัน')
+        return redirect('/createForm')
